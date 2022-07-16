0ba76cb695151cebf90f3428a14708c0350695f3
Anik Sarker <anik.sar@ascendcorp.com>
2022-07-17 03:34:04 +0800

    testing push #1

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ require('dotenv').config();
 
 // transporter
 const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'user@gmail.com',
-		pass: process.env.PASS, // naturally, replace both with your real credentials or an application-specific password
-	},
+  service: 'gmail',
+  auth: {
+    user: 'user@gmail.com',
+    pass: process.env.PASS, // naturally, replace both with your real credentials or an application-specific password
+  },
 });
 
 // mailOptions
 const mailOptions = {
-	from: 'sender@gmail.com',
-	to: 'receiver@gmail.com',
-	subject: 'Node Mailer Tester',
-	text: `Hi,
-        Anik,
+  from: 'sender@gmail.com',
+  to: 'receiver@gmail.com',
+  subject: 'Node Mailer Tester',
+  text: `Hi,
         How are you? Hope so you are doing well. This mail is sent using nodemailer.
 
         Thank You
@@ -26,9 +25,9 @@ const mailOptions = {
 
 // send mail
 transporter.sendMail(mailOptions, (error, info) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('Email sent: ' + info.response);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
